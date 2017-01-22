cd45c9499d8570f534bb4b318662b6da69672657
Amanda Gilmour <amandagilmour@amandas-macbook-air.local>
2017-01-22 14:55:10 -0500

    Initial Commit

---
+++ You Are Awesome 3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  You Are Awesome 3
+//
+//  Created by Amanda Gilmour on 1/22/17.
+//  Copyright © 2017 Amanda Gilmour. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

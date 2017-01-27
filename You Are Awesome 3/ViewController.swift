9998afb6bbf9f085ed85490e7b4eb0fbc8834d8f
Amanda Gilmour <amandagilmour@amandas-macbook-air.local>
2017-01-27 16:44:21 -0500

    Hw assignment 1

--- You Are Awesome 3/ViewController.swift
+++ You Are Awesome 3/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var messageButton: UIButton!
+    
+    var lastIndex = -1
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +24,58 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func messageButtonPressed(_ sender: UIButton) {
+        
+        let messages = ["You Are Fantastic!!!!",
+                        "You Are Great!",
+                        "You Are Amazing!",
+                        "You Are Awesome!",
+                        "When the genius bar needs help, they call you!",
+                        "You brighten my day!",
+                        "I can't wait to use your app!",
+                        "You are da bomb!"]
+        
+        var randomIndex: Int =
+            Int(arc4random_uniform(UInt32(messages.count)))
+        
+        while randomIndex == lastIndex {
+            randomIndex = Int(arc4random_uniform(UInt32(messages.count)))
+        }
+        
+        messageLabel.text = messages[randomIndex]
+        
+        lastIndex = randomIndex
+        
+        /*
+        messageLabel.text = messages[index]
+        
+        if index == messages.count - 1 {
+            index = 0
+        } else {
+            index = index + 1
+        }
+ */
+        
+        
+        
+        /*
+        let message1 = "You Are Fantastic!!!!"
+        let message2 = "You Are Great!"
+        let message3 = "You Are Amazing!"
+        
+        
+        if messageLabel.text == message1 {
+            messageLabel.text = message2 }
+        else if messageLabel.text == message2 {
+            messageLabel.text = message3
+        }
+        else {
+            messageLabel.text = message1
+        }
+ */
+        
+        }
+    }
 
-}
+
 
